c4befa07296af2b6ccb027c0b031b856b4ec850d
surrepoff <alexander.bessonov548@gmail.com>
2022-02-24 20:25:05 +0700

    Update code

--- VPaHMI-lab2/RomanNumberException.cs
+++ VPaHMI-lab2/RomanNumberException.cs
@@ -4,5 +4,7 @@ namespace VPaHMI_lab2
 {
     internal class RomanNumberException : Exception
     {
+        public RomanNumberException() : base("ERROR") { }
+        public RomanNumberException(string message) : base(message) { }
     }
 }
